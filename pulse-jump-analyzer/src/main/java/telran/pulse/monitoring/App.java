94a30029ee3e590608ae7a6d80063f5e3fc7848c
DmitriyVolchenko <dmitriy.volchenko@gmail.com>
2024-11-02 20:35:42 +0200

    last commit before replacing VSCode

--- pulse-jump-analyzer/src/main/java/telran/pulse/monitoring/App.java
+++ pulse-jump-analyzer/src/main/java/telran/pulse/monitoring/App.java
@@ -1,6 +1,6 @@
 package telran.pulse.monitoring;
 
-import java.util.*;
+import java.util.Map;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
@@ -8,19 +8,21 @@ import com.amazonaws.services.lambda.runtime.events.models.dynamodb.AttributeVal
 
 public class App {
 	public void handleRequest(DynamodbEvent event, Context context) {
-		event.getRecords().forEach(r -> {
-			Map<String, AttributeValue> map = r.getDynamodb().getNewImage();
-			if (map == null) {
-				System.out.println("No new image found");
-			} else if(r.getEventName().equals("INSERT")){
-				System.out.printf("patientId=%s, timestamp=%s, pulseValue=%s\n",
-						map.get("patientId").getN(), map.get("timestamp").getN(),
-						map.get("value").getN());
-
-			} else {
-				System.out.println(r.getEventName());
-			}
-
-		});
+		if (event.getRecords() != null && !event.getRecords().isEmpty()) {
+			event.getRecords().forEach(r -> {
+				Map<String, AttributeValue> map = r.getDynamodb().getNewImage();
+				if (map == null) {
+					System.out.println("No new image found");
+				} else if (r.getEventName().equals("INSERT")) {
+					System.out.printf("patientId=%s, timestamp=%s, pulseValue=%s\n",
+							map.get("patientId").getN(), map.get("timestamp").getN(),
+							map.get("value").getN());
+				} else {
+					System.out.println(r.getEventName());
+				}
+			});
+		} else {
+			System.out.println("No records found in the event.");
+		}
 	}
 }
