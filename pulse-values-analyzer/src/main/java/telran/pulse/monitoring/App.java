12c79e17ba6db4e1f3c0d3fa6bf1943fe199fd29
DmitriyVolchenko <dmitriy.volchenko@gmail.com>
2024-11-12 19:23:38 +0200

    code after CW#67

--- pulse-values-analyzer/src/main/java/telran/pulse/monitoring/App.java
+++ pulse-values-analyzer/src/main/java/telran/pulse/monitoring/App.java
@@ -1,116 +1,161 @@
 package telran.pulse.monitoring;
 
+import java.util.*;
+import java.util.logging.*;
+
+import org.json.JSONObject;
+
 import java.io.IOException;
 import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.ConsoleHandler;
-import java.util.logging.Handler;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.net.URISyntaxException;
+import java.net.http.*;
+import java.net.http.HttpResponse.BodyHandlers;
 
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
 import com.amazonaws.services.lambda.runtime.events.models.dynamodb.AttributeValue;
-import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
+import software.amazon.awssdk.services.dynamodb.*;
 import software.amazon.awssdk.services.dynamodb.model.PutItemRequest;
-
+import software.amazon.awssdk.services.dynamodb.model.PutItemRequest.Builder;
 import static telran.pulse.monitoring.Constants.*;
-import org.json.JSONObject;
+
+record Range(int min, int max) {
+
+}
 
 public class App {
-    static DynamoDbClient clientDynamo = DynamoDbClient.builder().build();
-    static Logger logger = Logger.getLogger("pulse-values-analyzer");
-    
-    private static final String BASE_URL = System.getenv("BASE_URL");
-    static {
-        loggerSetUp();
-    }
+	static HttpClient httpClient = HttpClient.newHttpClient();
+	static String baseURL;
+	static DynamoDbClient client = DynamoDbClient.builder().build();
+	static Builder dynamoItemRequest;
+	static Logger logger = Logger.getLogger("pulse-value-analyzer");
+	static {
+		loggerSetUp();
+		baseURLSetUp();
+	}
+	static HashMap<String, Range> ranges = new HashMap<>();
 
-    public void handleRequest(DynamodbEvent event, Context context) {
-        var records = event.getRecords();
-        if (records == null) {
-            logger.severe("No records in the event");
-        } else {
-            records.forEach(r -> {
-                var newImage = r.getDynamodb().getNewImage();
-                if (newImage == null) {
-                    logger.warning("No new image found");
-                } else if ("INSERT".equals(r.getEventName())) {
-                    processPulseValue(newImage);
-                } else {
-                    logger.warning(r.getEventName() + " event name but should be INSERT");
-                }
-            });
-        }
-    }
+	public void handleRequest(DynamodbEvent event, Context context) {
+		dynamoItemRequest = PutItemRequest.builder().tableName(ABNORMAL_VALUES_TABLE_NAME);
 
-    private void processPulseValue(Map<String, AttributeValue> map) {
-        int value = Integer.parseInt(map.get(VALUE_ATTRIBUTE).getN());
-        String patientId = map.get(PATIENT_ID_ATTRIBUTE).getN();
-        logger.finer(getLogMessage(map));
+		event.getRecords().forEach(r -> {
+			Map<String, AttributeValue> map = r.getDynamodb().getNewImage();
+			if (map == null) {
+				logger.warning("No new image found");
+			} else if (r.getEventName().equals("INSERT")) {
+				processPulseValue(map);
+			} else {
+				logger.warning(String.format("The event isn't INSERT but %s", r.getEventName()));
+			}
 
-        Range range = fetchRangeForPatient(patientId);
-        if (range != null && (value > range.max || value < range.min)) {
-            logAndRecordAbnormalValue(map);
-        }
-    }
+		});
+	}
 
-    private Range fetchRangeForPatient(String patientId) {
-        HttpClient client = HttpClient.newHttpClient();
-        String url = String.format("%s?patientId=%s", BASE_URL, patientId);
-        HttpRequest request = HttpRequest.newBuilder(URI.create(url)).build();
+	private static void baseURLSetUp() {
+		baseURL = System.getenv(BASE_URL_ENV_NAME);
+		if (baseURL == null) {
+			logger.severe("Range provider URL doesn't exist");
+			throw new RuntimeException(BASE_URL_ENV_NAME + " Environment variable not set");
+		}
+		logger.config(BASE_URL_ENV_NAME + " is " + baseURL);
 
-        try {
-            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
-            if (response.statusCode() == 200) {
-                JSONObject json = new JSONObject(response.body());
-                int min = json.getInt("min");
-                int max = json.getInt("max");
-                return new Range(min, max);
-            } else {
-                logger.warning("Failed to fetch range: " + response.body());
-            }
-        } catch (IOException | InterruptedException e) {
-            logger.log(Level.SEVERE, "Error fetching range from range-provider", e);
-        }
-        return null;
-    }
+	}
 
-    private void logAndRecordAbnormalValue(Map<String, AttributeValue> map) {
-        logger.info(getLogMessage(map));
+	private static void loggerSetUp() {
+		Level loggerLevel = getLoggerLevel();
+		LogManager.getLogManager().reset();
+		Handler handler = new ConsoleHandler();
+		logger.setLevel(loggerLevel);
+		handler.setLevel(Level.FINEST);
+		logger.addHandler(handler);
+	}
 
-        Map<String, software.amazon.awssdk.services.dynamodb.model.AttributeValue> sdkMap = convertToSdkAttributeMap(map);
-        clientDynamo.putItem(PutItemRequest.builder()
-                .tableName(ABNORMAL_VALUES_TABLE_NAME)
-                .item(sdkMap)
-                .build());
-    }
+	private static Level getLoggerLevel() {
+		String levelStr = System.getenv()
+				.getOrDefault(LOGGER_LEVEL_ENV_VARIABLE, DEFAULT_LOGGER_LEVEL);
+		Level res = null;
+		try {
+			res = Level.parse(levelStr);
+		} catch (Exception e) {
+			res = Level.parse(DEFAULT_LOGGER_LEVEL);
+		}
+		return res;
+	}
 
-    private Map<String, software.amazon.awssdk.services.dynamodb.model.AttributeValue> convertToSdkAttributeMap(Map<String, AttributeValue> lambdaMap) {
-        Map<String, software.amazon.awssdk.services.dynamodb.model.AttributeValue> sdkMap = new HashMap<>();
-        lambdaMap.forEach((key, value) -> {
-            sdkMap.put(key, software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder().n(value.getN()).build());
-        });
-        return sdkMap;
-    }
+	private void processPulseValue(Map<String, AttributeValue> map) {
+		int value = Integer.parseInt(map.get(VALUE_ATTRIBUTE).getN());
+		String patientIdStr = map.get(PATIENT_ID_ATTRIBUTE).getN();
+		logger.finer(getLogMessage(map));
+		Range range = getRange(patientIdStr);
+		if (value > range.max() || value < range.min()) {
+			processAbnormalPulseValue(map);
+		}
+	}
 
-    private String getLogMessage(Map<String, AttributeValue> map) {
-        return String.format("PatientId: %s, Value: %s", 
-                map.get(PATIENT_ID_ATTRIBUTE).getN(), 
-                map.get(VALUE_ATTRIBUTE).getN());
-    }
+	private Range getRange(String patientIdStr) {
+		Range res = ranges.get(patientIdStr);
+		if (res != null) {
+			logger.finer("Range taken from cache " + res);
+		} else {
+			res = getRangeFromProvider(patientIdStr);
+			logger.fine("Range taken from provider API " + res);
+		}
+		return res;
 
-    private static void loggerSetUp() {
-        Level loggerLevel = Level.parse(System.getenv().getOrDefault(LOGGER_LEVEL_ENV_VARIABLE, DEFAULT_LOGGER_LEVEL));
-        logger.setLevel(loggerLevel);
-        Handler handler = new ConsoleHandler();
-        handler.setLevel(Level.FINEST);
-        logger.addHandler(handler);
-    }
+	}
 
-    record Range(int min, int max) {}
+	private Range getRangeFromProvider(String patientIdStr) {
+
+		HttpRequest request = HttpRequest.newBuilder(getURL(patientIdStr)).build();
+		HttpResponse<String> response;
+		try {
+			response = httpClient.send(request, BodyHandlers.ofString());
+		} catch (IOException | InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+		String bodyJSON = response.body();
+		if (response.statusCode() >= 400) {
+			throw new RuntimeException(bodyJSON);
+		}
+		return getRangeFromJSON(bodyJSON);
+	}
+
+	private Range getRangeFromJSON(String bodyJSON) {
+		JSONObject jsonObj = new JSONObject(bodyJSON);
+		return new Range(jsonObj.getInt(MIN_FIELD_NAME), jsonObj.getInt(MAX_FIELD_NAME));
+	}
+
+	private URI getURL(String patientIdStr) {
+		String uriStr = baseURL + "?patientId=" + patientIdStr;
+		logger.fine(uriStr + " is range provider URI");
+		try {
+			return new URI(uriStr);
+		} catch (URISyntaxException e) {
+			String errorMessage = uriStr + " wrong URI format";
+			logger.severe(errorMessage);
+			throw new RuntimeException(errorMessage);
+		}
+	}
+
+	private void processAbnormalPulseValue(Map<String, AttributeValue> map) {
+		logger.info(getLogMessage(map));
+		client.putItem(dynamoItemRequest.item(getPutItemMap(map)).build());
+	}
+
+	private Map<String, software.amazon.awssdk.services.dynamodb.model.AttributeValue> getPutItemMap(
+			Map<String, AttributeValue> map) {
+		Map<String, software.amazon.awssdk.services.dynamodb.model.AttributeValue> res = new HashMap<>();
+		res.put(PATIENT_ID_ATTRIBUTE, software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder()
+				.n(map.get(PATIENT_ID_ATTRIBUTE).getN()).build());
+		res.put(TIMESTAMP_ATTRIBUTE, software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder()
+				.n(map.get(TIMESTAMP_ATTRIBUTE).getN()).build());
+		res.put(VALUE_ATTRIBUTE, software.amazon.awssdk.services.dynamodb.model.AttributeValue.builder()
+				.n(map.get(VALUE_ATTRIBUTE).getN()).build());
+		return res;
+	}
+
+	private String getLogMessage(Map<String, AttributeValue> map) {
+		return String.format("patientId: %s, value: %s", map.get(PATIENT_ID_ATTRIBUTE).getN(),
+				map.get(VALUE_ATTRIBUTE).getN());
+	}
 }
--- pulse-values-analyzer/src/main/java/telran/pulse/monitoring/Constants.java
+++ pulse-values-analyzer/src/main/java/telran/pulse/monitoring/Constants.java
@@ -4,7 +4,11 @@ public interface Constants {
     String PATIENT_ID_ATTRIBUTE = "patientId";
     String TIMESTAMP_ATTRIBUTE = "timestamp";
     String VALUE_ATTRIBUTE = "value";
+    
     String ABNORMAL_VALUES_TABLE_NAME = "pulse_abnormal_values";
     String LOGGER_LEVEL_ENV_VARIABLE = "LOGGER_LEVEL";
     String DEFAULT_LOGGER_LEVEL = "INFO";
+    String BASE_URL_ENV_NAME="BASE_URL";
+    String MIN_FIELD_NAME = "min";
+    String MAX_FIELD_NAME = "max";
 }
